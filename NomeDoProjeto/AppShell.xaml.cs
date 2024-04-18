7004d1cc45548982c85aa26a91a3ccacb9df4ffa
amanda <amandaleticiaraymundo@gmail.com>
2024-04-18 11:23:43 -0300

    começou

---
+++ NomeDoProjeto/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NomeDoProjeto;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NomeDoProjeto/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace NomeDoProjeto;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
