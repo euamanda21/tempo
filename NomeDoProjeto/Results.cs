a71e2f917c39c3bbd33febfa43de1e2a34cd374a
amanda <amandaleticiaraymundo@gmail.com>
2024-04-18 11:46:13 -0300

    começou

---
+++ NomeDoProjeto/Results.cs
@@ -0,0 +1,18 @@
+namespace ForecastApp;
+
+public class Results
+{
+  public int Temp {get; set;}
+  public string Description {get; set;}
+  public int date {get; set;}
+  public int condition_cod {get; set;}
+  public int img_id {get; set;}
+  public string Currently {get; set;}
+  public string City {get; set;}
+  public int Humidity {get; set;}
+  public double Rain {get; set;}
+  public string Sunrise {get; set;}
+  public string Sunset {get; set;}
+  public string Wind_Speedy {get; set;}
+  public string Wind_Direction {get; set;}
+}
