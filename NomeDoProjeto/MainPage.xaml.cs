f3fd6d88d68478adc62e1aae2965a48b73efbed3
amanda <amandaleticiaraymunndo@gmail.com>
2024-04-23 09:00:20 -0300

    continuou

--- NomeDoProjeto/MainPage.xaml.cs
+++ NomeDoProjeto/MainPage.xaml.cs
@@ -1,24 +1,64 @@
 ﻿namespace NomeDoProjeto;
 
+public partial class MainPage : ContentPage;
+
+	
+	public MainPage();
+		InitializeComponent();
+
+	
+namespace tempo;
+
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	Results resultados;
+	int count=0;
+	public MainPage();
+    
 
-	public MainPage()
-	{
 		InitializeComponent();
-	}
+		TestaLayout();
+		preencherTela();
+	
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	void TestaLayout()
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+      resultados=new Results();
+	  resultados.Temp = 23;
+	  resultados.Description = "tempo nublado";
+	  resultados.Date = "22042024";
+	  resultados.Time = "0855";
+	  resultados.Currently ="dia";
+	  resultados.City = "Apucarana,PR";
+	  resultados.Humidity = "90";
+	  resultados.Rain = "10";
+	  resultados.Sunrise = "06:11 am";
+	  resultados.Sunset = "06:39 pm";
+	  resultados. Wind_Speedy = "4.99 km/h";
+	  resultados.Wind_Direction = "40";
+	  resultados.moon_phase = "cheia";
+	
+	  
 	}
+
+	void preencherTela()
+	{
+      labelTemperature.Text = resultados.Temp;
+	  labelDescription.Text = resultados.Description;
+	  labelDate.Text = resultados.Date;
+      labelTime.Text = resultados.Time;
+	  labelCurrently.Text = resultados.Currently;
+	  labelCity.Text = resultados.City;
+	  labelHumidity.Text = resultados.Humidity;
+	  labelRain.Text = resultados.Rain;
+	  labelSunrise.Text = resultados.Sunrise;
+	  labelSunset.Text = resultados.Sunset;
+	  labelWind_Speedy.Text = resultados. Wind_Speedy;
+	  labelWind_Direction.Text = resultados.Wind_Direction;
+	  labelmoon_phase.Text = resultados.moon_phase;
+    }
 }
+
+	
+
 
