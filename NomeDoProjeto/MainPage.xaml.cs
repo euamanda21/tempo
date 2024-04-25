9c1c111efe6e7a7282a6c184d9cb6840a74fcce8
amanda <amandaleticiaraymundo@gmail.com>
2024-04-25 11:48:54 -0300

    continuou

--- NomeDoProjeto/MainPage.xaml.cs
+++ NomeDoProjeto/MainPage.xaml.cs
@@ -1,46 +1,42 @@
-﻿namespace Tempo;
+﻿using TempoApp;
 
-public partial class MainPage : ContentPage;   	
+namespace Tempo;
 
-{   Resposta resposta;
+public partial class MainPage : ContentPage	
+{
+ 
+	Resposta resposta;
 
 	const string Url="https://apihgbrasil.com/weather?woieid=455927&key=618e34cf";
 
-	int count = 0;
+	
 	public MainPage()
 {
-		InitializeComponent();
-		TestaLayout();
-		preencherTela();
-}
 
-	void TestaLayout()
-	{
-      resultados=new Results();
-	  resultados.Temp =23;
-	  resultados.Description ="tempo nublado";
-	  resultados.Date =22042024;
-	  resultados.Time =0855;
-	  resultados.Currently ="dia";
-	  resultados.City = "Apucarana,PR";
-	  resultados.Humidity = "90";
-	  resultados.Rain ="10";
-	  resultados.Sunrise = "06:11 am";
-	  resultados.Sunset = "06:39 pm";
-	  resultados. Wind_Speedy = "4.99 km/h";
-	  resultados.Wind_Direction = 40;
-	  resultados.moon_phase = "cheia";
-	
-	  
+		
+		InitializeComponent();
+		preencherTela();	
+		
 	}
+
 
 	void preencherTela()
 	{
-      if (resultado.currently.rain>=10)
-	    imgBackground.Source = "diachuvoso.jpg";
+        Labelchuva.Text= resposta.results.Temp.ToString();
+	    ImgBackground.Source = "diachuvoso.jpg";
+        LabelSol.Text= resposta.results.Temp.ToString();
+	    ImgBackground.Source = "diasol.jpg";
+        LabelNublado.Text= resposta.results.Temp.ToString();
+	    Imgbackground.Source = "dianublado.jpg";
+        Labelnoitechuva.Text= resposta.results.Temp.ToString();
+	    ImgBackground.Source = "noitechuvosa.jpg";
+        Labelnoiteestrelada.Text= resposta.results.Temp.ToString();
+	    ImgBackground.Source = "noiteestrelada.jpg";
+        Labelnoitenublada.Text= resposta.results.Temp.ToString();
+	    ImgBackground.Source = "noitenublada.jpg";
     }
-}
 
 	
 
 
+}
